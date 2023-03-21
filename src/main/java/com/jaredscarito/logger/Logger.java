a92f7f7d11b98192aa24201cec1f9ddfc484ce75
Jared <TheWolfBadger@gmail.com>
2023-03-20 21:31:32 -0400

    + Add functionality to Logger

--- src/main/java/com/jaredscarito/logger/Logger.java
+++ src/main/java/com/jaredscarito/logger/Logger.java
@@ -1,6 +1,43 @@
 package com.jaredscarito.logger;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class Logger {
+    private static String getCurrentDatetimeString() {
+        Date date = new Date();
+
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        formatter.setTimeZone(TimeZone.getTimeZone("EST"));
+
+        return (formatter.format(date));
+    }
     public static void log(String action, String performedBy, String performedOn, String reasoning) {}
-    public static void log(Exception ex) {}
+    public static void log(Exception ex) {
+        File errors = new File("logs/error.txt");
+        if (!errors.exists()) {
+            try {
+                errors.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (errors.exists()) {
+            try {
+                FileWriter writer = new FileWriter(errors, true);
+                writer.write("[" + getCurrentDatetimeString() + "] Error Encountered:");
+                writer.write(ex.getMessage());
+                writer.write("\n");
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
